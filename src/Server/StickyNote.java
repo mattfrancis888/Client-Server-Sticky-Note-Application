03ac384124656fc8a483499152353b1f567c2abd
Matthew Francis <mattfrancis888@gmail.com>
2021-09-30 21:44:08 -0400

    Created StickyNote with properties, getter, setters.

--- src/Server/StickyNote.java
+++ src/Server/StickyNote.java
@@ -3,5 +3,59 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class StickyNote {
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    private String color = "";
+    private int x = 0;
+    private int y = 0;
+    private String name = "";
+    private int width = 0;
+    private int  height = 0;
 
 }
