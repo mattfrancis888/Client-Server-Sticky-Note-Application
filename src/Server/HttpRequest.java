5d69789c2ef43dd9824b4519e0b5cd24d0b8ec55
Matthew Francis <mattfrancis888@gmail.com>
2021-09-30 22:34:13 -0400

    Set up HTTP request class.

---
+++ src/Server/HttpRequest.java
@@ -0,0 +1,127 @@
+package Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+
+public class HttpRequest {
+    private final Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+    private final ArrayList<StickyNote> bookEntries;
+
+    public ServerThread(String name, Socket socket, ArrayList<StickyNote> stickyNoteEntries) {
+        super(name);
+        this.socket = socket;
+        this.bookEntries = stickyNoteEntries;
+    }
+
+    public void run() {
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            listen();
+//            disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String processData(String[] data) {
+        //POST,GET,PIN,UNPIN,CLEAR,SHAKE,
+        final String request = data[0].trim();
+        switch (request) {
+            case "POST":
+                return handlePost(data);
+//            case "GET":
+//                return handleGet(data);
+//            case "PIN":
+//                return handlePin(data);
+//            case "UNPIN":
+//                return handleUnpin(data);
+//            case "CLEAR":
+//                return handleClear(data);
+//            case "SHAKE":
+//                return handleShake(data);
+            default:
+                return "ERROR - Request is not a POST, GET, PIN, UNPIN, CLEAR, SHAKE";
+        }
+    }
+
+    private void listen() {
+        String line, inMessage, outMessage;
+        try {
+            //Read everything line by line from the input
+            line = in.readLine();
+            while (line != null) {
+                inMessage = "";
+                if (line.equals("ping")) { //COME BACK
+                    outMessage = "pong";
+                } else {
+                    /* READ DATA START */
+                    while (!line.contains("\\EOF")) {
+                        inMessage = inMessage.concat(line + "\r\n");
+                        line = in.readLine();
+                    }
+                    /* READ DATA END */
+
+                    /* PROCESS DATA START*/
+                    outMessage = processData(inMessage.split("\n")).trim() + "\r\n\\EOF";
+                }
+                out.println(outMessage);
+                line = in.readLine();
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+
+    //handle stuff
+    private String handlePost(String[] data) {
+        String message;
+        StickyNote stickyNote = new StickyNote();
+        for (String line : data) {
+            line = line.trim();
+            String[] words = line.split(" ");
+            String value;
+            switch (words[0]) {
+                case "COLOR":
+                    if (Util.findByISBN(bookEntries, words[1]) != null) {
+                        message = "ERROR: Book already exists";
+                        return message;
+                    }
+                    stickyNote.setColor(words[1]);
+                    break;
+                case "X":
+                    value = line.substring(words[0].length()).trim();
+                    stickyNote.setX(value);
+                    break;
+                case "Y":
+                    value = line.substring(words[0].length()).trim();
+                    stickyNote.setY(value);
+                    break;
+                case "NAME":
+                    value = line.substring(words[0].length()).trim();
+                    stickyNote.setName(value);
+                    break;
+                case "WIDTH":
+                    stickyNote.setWidth(Integer.parseInt(words[1]));
+                    break;
+                case "HEIGHT":
+                    stickyNote.setHeight(Integer.parseInt(words[1]));
+                    break;
+                default:
+                    break;
+            }
+        }
+        message = "-----Successfully added-----\n" + bookEntry.toString();
+        bookEntries.add(bookEntry);
+        return message;
+    }
+
+}
